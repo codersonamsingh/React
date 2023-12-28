e466b0929f461f1af045403d37b47b92b515fa53
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-12-28 12:34:08 +0530

    custom ract running

--- 01basicreact/customReact/customReact.js
+++ 01basicreact/customReact/customReact.js
@@ -1,11 +1,16 @@
 
 function customRender(reactElement, container){
+    /*
     const domElement = document.createElement
     (reactElement.type)
-    domElement.innerHtml = reactElement.children
+    domElement.innerHTML = reactElement.children
     domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target',reactElement.props.target)
+
+    container.appendChild(domElement)
+    /*
 }
+
 
 const reactElement = {
     type:'a',
