12554a1f44a0a0663f6000a7581a54c9f169f164
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-12-29 21:36:05 +0530

    raturn running

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-'>Tailwind test</h1>
+      <h1 className='bg-green-400 text-black p-4
+      rounded-xl'>Tailwind test</h1>
     </>
   )
 }
