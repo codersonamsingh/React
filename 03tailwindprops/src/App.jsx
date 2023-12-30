c527e1cf60972a72cccc0f1fe07e63e88d7d5a36
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-12-30 21:29:25 +0530

    App card.jsx running

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    username: "sonam",
+    age: 21
+  }
+
+   let newArr = {1, 2, 3}
+ 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwindtext test</h1>
+      <Card username="sonamorcode" />
+      <Card/>
+      <Card/>
      </>
   )
 }
