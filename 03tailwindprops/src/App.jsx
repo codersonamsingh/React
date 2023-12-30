e0fe6feb86b20085cef91537a7c2a080a08c7514
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-12-30 16:16:56 +0530

    devil.li running

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -10,9 +10,25 @@ function App() {
     <>
       <h1 className='bg-green-400 text-black p-4
       rounded-xl'>Tailwind test</h1>
-      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        
-      </figure>
+      <div class="relative h-[400px] w-[300px] rounded-md">
+  <img
+    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+    alt="AirMax Pro"
+    class="z-0 h-full w-full rounded-md object-cover"
+  />
+  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+  <div class="absolute bottom-4 left-4 text-left">
+    <h1 class="text-lg font-semibold text-white">Delba</h1>
+    <p class="mt-2 text-sm text-gray-300">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+      debitis?
+    </p>
+    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+      View Profile →
+    </button>
+  </div>
+</div>
+
      </>
   )
 }
