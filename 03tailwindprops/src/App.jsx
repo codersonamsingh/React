92f91c88b20f5bf8242cc9a097a25f451f56a3c8
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-12-30 21:38:26 +0530

    jsx running

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -12,14 +12,14 @@ function App() {
     age: 21
   }
 
-   let newArr = {1, 2, 3}
+   //let newArr = {1,2,3}
  
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwindtext test</h1>
       <Card username="sonamorcode" />
-      <Card/>
-      <Card/>
+      <Card username="sonam"/>
+      
      </>
   )
 }
