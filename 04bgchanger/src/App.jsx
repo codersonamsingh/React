aa83e69e71cadbb76f1bc10fe9af02c43725f472
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-12-31 23:02:52 +0530

    tailwind App,jsx running

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 function App() {
   const [] = useState()
   return (
-    <>
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: "#000"}}
+    >
       
     </>
   )
