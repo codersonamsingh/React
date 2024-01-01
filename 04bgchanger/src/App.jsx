c4b74f3ef5cccb5638bb4b1b857f5bceeaca52bd
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-01 21:05:42 +0530

    div running

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -7,7 +7,13 @@ function App() {
     style={{backgroundColor: color}}
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={}
+          ></button>
+        </div>
 
       </div>
     </div>
