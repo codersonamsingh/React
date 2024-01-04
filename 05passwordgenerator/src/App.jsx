9a9bb5cf76f61c30bb24103773fd0dcce22514d2
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-04 16:39:08 +0530

    str pass chart running

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -8,7 +8,21 @@ function App() {
   (false);
   const [charAllowed, setCharAllowed] = useState(false)
   const [Password, setPassword] = useState ("")
-  const PasswordGenerator = useCallback(fn, [])
+  const PasswordGenerator = useCallback(() => {
+    let pass = ""
+    let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*(){}[]"
+
+    for (let i = 1; i <= Array.length; i++){
+      let char = Math.floor(Math.random() * str.length +1)
+      pass = str.charAt(char)
+    }
+
+
+
+  }
+    , [length, numberAllowed, charAllowed, setPassword])
  
 
   return (
