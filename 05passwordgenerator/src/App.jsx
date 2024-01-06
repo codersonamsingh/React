3862f0ab1a4ff540fa5df5df338d643433ba14b5
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-06 16:34:42 +0530

    char art running

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
     for (let i = 1; i <= length; i++){
       let char = Math.floor(Math.random() * str.length +1)
-      pass += str.charAt()
+      pass += str.charAt(char)
     }
     setPassword (pass)
 
