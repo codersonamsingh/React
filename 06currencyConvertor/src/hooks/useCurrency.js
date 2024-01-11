c720f6466016ca49a54d6954337252c9c19b5ae2
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-11 18:28:42 +0530

    password running

--- 06currencyConvertor/src/hooks/useCurrency.js
+++ 06currencyConvertor/src/hooks/useCurrency.js
@@ -3,7 +3,8 @@ import { useEffect } from "react";
 
 function useCurrencyInfo(currency){
     useEffect (() => {
-        fetch()
+        fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json'
+        )
         .then ((res) => res.json())
         .then ((res) => setData (res[currency]))
         console.log(data);
