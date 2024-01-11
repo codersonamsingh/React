0b30d63fec39425e8ea33810cd50b922ab41d998
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-11 17:53:50 +0530

    usecurrency.js running

--- 06currencyConvertor/src/hooks/useCurrency.js
+++ 06currencyConvertor/src/hooks/useCurrency.js
@@ -4,7 +4,13 @@ import { useEffect } from "react";
 function useCurrencyInfo(currency){
     useEffect (() => {
         fetch()
-    })
+        .then ((res) => res.json())
+        .then ((res) => setData (res[currency]))
+        console.log(data);
+        
+    }, [currency])
+    console.log(data);
+    return data
 }
 
 
