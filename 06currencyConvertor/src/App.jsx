838c6d4d11a43d28643e6eb7c56b7c3d48d061be
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-13 17:43:11 +0530

    back imasg

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -25,18 +25,52 @@ function App() {
    setConvertedAmount(amount * currencyInfo[to])
  }
 
-  
 
-
-
-
-
-  return (
-    <>
-      <h1 className='text-3xl bg-orange-500'> Currency app with react</h1>
-      
-    </>
-  )
+ return (
+  <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+          backgroundImage: `url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2F
+          search%2Fbeautiful%2F&psig=AOvVaw0QRa3DGO-UBC-MMvcfm3ah&ust=1705233030401000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNjrvp-m2oMDFQAAAAAdAAAAABAD')`,
+      }}
+  >
+      <div className="w-full">
+          <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+              <form
+                  onSubmit={(e) => {
+                      e.preventDefault();
+                     
+                  }}
+              >
+                  <div className="w-full mb-1">
+                      <InputBox
+                          label="From"
+                          
+                      />
+                  </div>
+                  <div className="relative w-full h-0.5">
+                      <button
+                          type="button"
+                          className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                          
+                      >
+                          swap
+                      </button>
+                  </div>
+                  <div className="w-full mt-1 mb-4">
+                      <InputBox
+                          label="To"
+                          
+                      />
+                  </div>
+                  <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                      Convert 
+                  </button>
+              </form>
+          </div>
+      </div>
+  </div>
+);
 }
 
 export default App
