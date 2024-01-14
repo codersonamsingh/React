49ed9ed6d4ba00c521b81ada5b24ff5af69eb70a
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-14 14:29:56 +0530

    info running

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { InputBox } from './components'
-import useCurrencyInfo from './useCurrencyInfo'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 import './App.css'
 
