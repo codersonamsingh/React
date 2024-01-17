28d973471ebee057cf5d3144bd0329c9b5fe9a11
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-17 15:46:58 +0530

    dafult github running

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { resolvePath } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/codersonamsingh')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            setData(data)
-        })
-    }, [])
+    const data = useLoaderData()
+    //const [data, setData] = useState([])
+    //useEffect(() => {
+      //  fetch('https://api.github.com/users/codersonamsingh')
+        //.then(response => response.json())
+        //.then(data => {
+          //  console.log(data);
+            //setData(data)
+        //})
+    //}, [])
     return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}</div>
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+        <img src={data.avtar_url} alt="Git picture" width={300} />
+        </div>
     )
 }
 
