73b76aac70fd16d152f29586628f5459756c8d85
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-18 15:11:47 +0530

    App.jsx usar contaxt providar running

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -4,10 +4,11 @@ import UserContext from "./UserContext";
 const  UserContextProvider = ({children}) => {
     const [user, setUser] = React.useState(null)
     return(
-        <UserContext.Provider value={}>
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
             
         </UserContext.Provider>
     )
 
 }
-export default UserContextProvider
+export default UserContextProvider 
