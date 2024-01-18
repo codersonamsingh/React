5f34723e9822e832b03504b890d89a49f5083a8e
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-18 18:06:43 +0530

    profil .jsx running

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -4,9 +4,10 @@ import UserContext from '../context/UserContext'
 function Profile() {
     const {setUser} = useContext(UserContext)
 
-    return (
-        <div>Profile</div>
-    )
+    if(!user) return <div>please login</div>
+    return 
+        <div>Welcom {user.username}</div>
+    
 }
 
 export default Profile
