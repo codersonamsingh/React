06f6915febe4412090f35f4b4613c3a53557f29d
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-19 21:17:34 +0530

    App.jsx running

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* themeBtn */}
+            {ThemeBtn }
         </div>
 
         <div className="w-full max-w-sm mx-auto">
