2b689b39762b586af90556f787b65ef9f721a388
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-20 20:55:19 +0530

    import html running

--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,1 +1,18 @@
-import React from 'react
+import {createContext, useContext,} from "react"
+
+export const TodoContext = createContext({
+    todos:[
+        {   (property) id: number
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+
+        }
+    ],
+    theme:dark
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+export const Todoprovider = TodoContext.Provider
