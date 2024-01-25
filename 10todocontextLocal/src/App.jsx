98c7a6b3093a844466841f7c99879fa99e4f863e
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-01-25 23:41:36 +0530

    us todos running

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -21,6 +21,17 @@ function App() {
     prevTodo)
     )
   }
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+  
+  if (todos && todos.length > 0){
+    setTodos(todos)
+}
+}, [])
+
+useEffect(() => {
+  localStorage.setItem("todos", JSON.stringify(todos))
+}, [todos])
 
 
 
