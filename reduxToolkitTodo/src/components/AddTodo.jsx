ae7125c20ed38e761846e75eb1edb3a3d381123d
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-02-21 16:01:58 +0530

    jsx todo running

--- reduxToolkitTodo/src/components/AddTodo.jsx
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,9 +1,18 @@
 import React, {useState} from "react";
+import {useDispatch} from 'react-redux'
+import { addTodo } from "../feature/todo/todoSlice"
 
 
  function AddTodo () {
      
     const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
+    }
 
     return (
         <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
--- reduxToolkitTodo/src/components/Todos.jsx
+++ reduxToolkitTodo/src/components/Todos.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../feature/todo/todoSlice'
 
 function Todos (){
+
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+
     return (
+        <>
         <div>Todos</div>
+        {todos.map((todo) => (
+            <li key={todo.id}>
+                {todo.text}
+            </li>
+        ))}
+        
+        </>
     )
 }
 export default Todos
